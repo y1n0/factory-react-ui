c8e3a6777eb390f71653fee236c577727d07f049
Saber Ouarezzamen <our.saber@gmail.com>
2020-03-23 16:50:54 +0100

    #15923# Setup storybook

---
+++ src/0-Welcome.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import App from './App';
+
+export default {
+  title: 'Application',
+  component: App
+};
+
+export const Application = () => <App></App>;
+
+Application.story = {
+  name: 'Void Factory',
+};
