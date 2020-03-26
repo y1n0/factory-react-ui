3830c8e5d7345e89efbc2cf79e6d15b88f8f4fa4
Saber Ouarezzamen <our.saber@gmail.com>
2020-03-26 18:02:23 +0100

    #15924 CSS Reset

--- src/components/GlobalStyle/index.js
+++ src/components/GlobalStyle/index.js
@@ -3,8 +3,29 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
 
+    *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
+    }
+
+    body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+        margin: 0;
+        padding: 0;
+        font-weight: normal;
+    }
+
+    ol, ul {
+        list-style: none;
+    }
+
+    img {
+        max-width: 100%;
+        height: auto;
+    }
+
     html {
-        font-family: ${({theme}) => theme.fonts.sans};
+        font-family: ${({ theme }) => theme.fonts.sans};
         font-size: 16px;
         box-sizing: border-box;
     }
