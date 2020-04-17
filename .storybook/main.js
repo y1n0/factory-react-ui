64c8bb653d54f435f8097b9ee6b59143308cd521
Saber Ouarezzamen <our.saber@gmail.com>
2020-04-17 15:18:22 +0100

    enable addon-viewport

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,7 @@ module.exports = {
     '@storybook/addon-knobs/register',
     '@storybook/addon-storysource',
     '@storybook/addon-notes/register',
-    // '@storybook/addon-viewport/register',
+    '@storybook/addon-viewport/register',
     '@storybook/addon-backgrounds/register'
   ],
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -9,12 +9,12 @@ import GlobalStyle from "../src/components/GlobalStyle";
 import { addParameters } from '@storybook/react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
-// addParameters({
-//         viewport: {
-//           viewports: INITIAL_VIEWPORTS, // newViewports would be an ViewportMap. (see below for examples)
-//           defaultViewport: 'responsive',
-//         },
-//       });
+addParameters({
+        viewport: {
+          viewports: INITIAL_VIEWPORTS, // newViewports would be an ViewportMap. (see below for examples)
+          defaultViewport: 'responsive',
+        },
+      });
 
 addDecorator(storyFn =>  <ThemeProvider theme={theme}>
         <ExpirementalDefaultFont />
