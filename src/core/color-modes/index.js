30aef4f0360e26c6cc3fc89ff26c9c6a0871c7d2
Saber Ouarezzamen <our.saber@gmail.com>
2020-05-03 02:29:38 +0000

    enable localstorage to store color mode

--- src/core/color-modes/index.js
+++ src/core/color-modes/index.js
@@ -2,39 +2,65 @@ import React, { useState, useEffect, useContext } from 'react';
 import { ThemeContext, ThemeProvider } from 'styled-components';
 import { get } from 'styled-system';
 import deepmerge from 'deepmerge';
-import {useVactoryTheme, VactoryThemeContext} from '../index';
+import { useVactoryTheme, VactoryThemeContext } from '../index';
 
 
-// const STORAGE_KEY = 'vactory-ui-color-mode';
+const STORAGE_KEY = 'vactory-ui-color-mode';
+
+const storage = {
+    get: init => {
+        try {
+            return window.localStorage.getItem(STORAGE_KEY) || init
+        } catch (e) {
+            console.warn(
+                'localStorage is disabled and color mode might not work as expected.',
+                'Please check your Site Settings.',
+                e
+            )
+        }
+    },
+    set: value => {
+        try {
+            window.localStorage.setItem(STORAGE_KEY, value)
+        } catch (e) {
+            console.warn(
+                'localStorage is disabled and color mode might not work as expected.',
+                'Please check your Site Settings.',
+                e
+            )
+        }
+    },
+}
 
 const applyColorMode = (theme, mode) => {
     if (!mode) return theme
     const modes = get(theme, 'colors.modes', {})
     return deepmerge.all([{}, theme, {
-      colors: get(modes, mode, {}),
+        colors: get(modes, mode, {}),
     }]);
-  }
+}
 
-  
+
 const useColorModeState = (theme = {}) => {
 
-    // TODO : enable to set init color Mode
     const [mode, setMode] = useState('default');
 
     // init state
     useEffect(() => {
-        // get and set stored color mode 
-    }, []);
+        const stored = storage.get();
+        if (!stored || stored === mode) return;
+        setMode(stored);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [] );
 
     useEffect(() => {
-
-        // store mode
-
-    }, []);
+        if (!mode) return
+        storage.set(mode)
+    }, [mode]);
 
     return [mode, setMode];
 
-}  
+}
 
 export const useColorMode = () => {
     const { colorMode, setColorMode } = useVactoryTheme();
@@ -42,30 +68,29 @@ export const useColorMode = () => {
         throw new Error(`[useColorMode] requires the ColorModeProvider component`)
     }
 
-
     return [colorMode, setColorMode];
 }
 
 
-export const ColorModeProvider = ({children}) => {
+export const ColorModeProvider = ({ children }) => {
 
     const styledComponentTheme = useContext(ThemeContext);
 
     const [colorMode, setColorMode] = useColorModeState(styledComponentTheme);
 
-    const theme = applyColorMode(styledComponentTheme || {}, colorMode);
+    const theme = applyColorMode(styledComponentTheme || {}, colorMode);
 
     const context = {
         theme,
         colorMode,
         setColorMode,
-      }
+    }
 
     return (
         <ThemeProvider theme={theme}>
             <VactoryThemeContext.Provider value={context}>
                 {children}
             </VactoryThemeContext.Provider>
-        </ThemeProvider> 
+        </ThemeProvider>
     );
 }
