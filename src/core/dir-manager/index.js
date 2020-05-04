130a3380b4148a0592b9514a6fb68b4367700f65
Saber Ouarezzamen <our.saber@gmail.com>
2020-05-04 11:27:51 +0000

    fix fragment

--- src/core/dir-manager/index.js
+++ src/core/dir-manager/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { StyleSheetManager } from "styled-components";
 import stylisRTLPlugin from 'stylis-plugin-rtl';
 
 
 export const DirectionManager = ({dir= "ltr", children}) => {
-    return <StyleSheetManager stylisPlugins={dir === 'rtl' ? [stylisRTLPlugin] : []}><>{children}</></StyleSheetManager>
+    return <StyleSheetManager stylisPlugins={dir === 'rtl' ? [stylisRTLPlugin] : []}><Fragment>{children}</Fragment></StyleSheetManager>
 } 
