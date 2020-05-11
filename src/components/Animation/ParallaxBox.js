d0604f9c78c327b382e734b9b0ddbb99fd032a2e
Saber Ouarezzamen <our.saber@gmail.com>
2020-05-11 13:13:22 +0000

    #15949 Design System/Parallax

---
+++ src/components/Animation/ParallaxBox.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Box } from '../Box';
+import { useViewportScroll, useTransform } from 'framer-motion';
+import { MotionBox } from './MotionBox';
+
+
+export const ParallaxBox = ({
+    as = MotionBox, 
+    children,
+    easing = [0.42, 0, 0.58, 1],
+    ...rest }) => {
+
+    const [elementTop, setElementTop] = React.useState(0);
+
+    const ref = React.useRef(null);
+    const { scrollY } = useViewportScroll();
+    
+
+    React.useEffect(() => {
+        if (!ref.current) return;
+        const setValues = () => {
+            setElementTop(ref.current.offsetTop);
+        };
+      
+        setValues();
+
+        document.addEventListener("load", setValues);
+        window.addEventListener("resize", setValues);
+
+        return () => {
+            document.removeEventListener("load", setValues);
+            window.removeEventListener("resize", setValues);
+        };
+
+    }, [ref]);
+
+    const transformInitialValue = elementTop - 1; 
+    const transformFinalValue = elementTop + 1;
+    const yRange = [transformInitialValue, transformFinalValue];
+
+    const y = useTransform(scrollY, yRange, [0, -1], {
+        clamp: false,
+        easing
+    });
+    return <Box as={as} ref={ref}  style={{ y }} {...rest}>{children}</Box>
+}
--- src/stories/animation.stories.js
+++ src/stories/animation.stories.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import { Box, Heading } from '../components';
-import { motion } from 'framer-motion';
+import { Box, ParallaxBox } from '../components';
 
-const MotionBox = motion.custom(Box);
-const ParallaxBox = ({children, ...props}) => {
-    
-    const [elementTop, setElementTop] = React.useState(0);
-
-
-    return <MotionBox>{children}</MotionBox>
-}
 export const Parallax = () => {
 
 
-    return (<Box>
-        <Heading level="1">Example from framer.io/motion</Heading>
+    return (<Box height="200vh">
+        <Box bg='gray200' height="90px" p="large" border="2px solid" borderColor="gray500" >
+        </Box>
+
+        <ParallaxBox sx={{
+            width: '200px',
+            height: '300px',
+            color: 'white',
+            alignItems: 'center',
+            fontSize: '52px',
+            justifyContent: 'center',
+            backgroundColor: 'primary500'
+        }} >1</ParallaxBox>
+
+        <Box bg='gray200'p="large" border="2px solid" borderColor="gray500" >
+            <ParallaxBox  sx={{
+                width: '100%',
+                height: '150px',
+                alignItems: 'center',
+                fontSize: '52px',
+                justifyContent: 'center',
+                color: 'white',
+                backgroundColor: 'success300'
+            }} >2</ParallaxBox>
+        </Box>
     </Box>)
 }
 
