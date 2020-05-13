8120f3da84f5abaeaf873c9dcbe5afeaf4d90a0a
Saber Ouarezzamen <our.saber@gmail.com>
2020-05-13 13:32:42 +0000

    #17178: Design System/Label: fix import

--- src/components/Form/Label.js
+++ src/components/Form/Label.js
@@ -2,10 +2,8 @@ import React from 'react';
 import {Flex} from '../Box'
 
 
-const Label = ({children, variant, ...props}) => {
+export const Label = ({children, variant, ...props}) => {
     return <Flex as="label" {...props} variant={variant}>
         {children}
     </Flex>
 }
-
-export default Label;
