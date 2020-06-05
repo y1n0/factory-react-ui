915880b66eb936c942d73c1b187a8b060dfca7d0
Saber Ouarezzamen <our.saber@gmail.com>
2020-06-05 13:10:02 +0100

    update match-media

--- src/core/match-media/match-media.js
+++ src/core/match-media/match-media.js
@@ -1,14 +1,24 @@
 import {useState, useEffect, useContext} from 'react'
 import { ThemeContext } from 'styled-components';
+import {stripUnit} from 'polished';
+
+
+const getClosestValue = (val, arr) => {
+  const index = arr.sort((a,b) => a - b).findIndex(el => val < el);
+  return index > 0 ? arr[index] : null;
+}
 
 export const useMedia = mediaQuery => {
     const [doesMatch, onSetDoesMatch] = useState(false);
     const {breakpoints} = useContext(ThemeContext) || {};
     const breakpointsKeys = Object.keys(breakpoints).map(bp => isNaN(bp) ? bp : +bp); 
     let _query = mediaQuery;
-
+    
     if(breakpointsKeys.includes(mediaQuery)) {
-        _query = `screen and (min-width: ${breakpoints[mediaQuery]})`;
+        const sortedBreakpointsValues = breakpoints.map(e => stripUnit(e))
+        const minWidth = breakpoints[mediaQuery];
+        const maxWidth = getClosestValue(stripUnit(minWidth), sortedBreakpointsValues);
+        _query = `(min-width: ${minWidth})${maxWidth !== null ? `and (max-width: ${maxWidth}px)` : '' }`;
     }
 
     useEffect(() => {
