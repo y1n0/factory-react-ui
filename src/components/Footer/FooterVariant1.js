f5edca6752722aa545a7a9484adf4684f4a8b54e
Abdelkoddous <a.elidrissi@void.fr>
2020-06-11 11:38:13 +0100

    Footer - Variante1: adapt capital azur footer

---
+++ src/components/Footer/FooterVariant1.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+import { Footer } from './Footer';
+import { Box, Flex } from '../Box';
+import { Navs, Nav } from '../Navs';
+import { Icon} from '../Icon';
+
+
+const FooterNav = props =>  <Nav sx={{
+    color: 'inherit',
+    p: 0,
+    px: [0, '16px'],
+    borderRight: ['0', '1px solid rgba(0,0,0,0.19)'],
+    m:0,
+    mb: ['12px', '0'],
+    '&:last-child' : {
+        borderRight: 0,
+        pr: 0,
+        m: 0
+    }
+    }}>{props.children}</Nav>
+
+const FooterNavs = props =>  <Navs sx={{
+        p: 0,
+        m:0,
+        width:"100%",
+        alignItems: 'baseline'
+        }}>{props.children}</Navs>
+
+export const FooterVariant1 = () => {
+    return (
+        <Footer sx={{
+            backgroundColor: '#fff',
+            fontFamily: 'montserrat',
+            display: 'flex',
+            flexDirection: 'column'
+        }}>
+            <Flex sx={{
+                width: ['100%', '100%', '900px', '100%', '100%'],
+                display: 'flex',
+                flexDirection: 'column',
+            }}>
+                <Flex sx={{
+                    fontSize: "14px",
+                    color: '#000000',
+                    flexDirection: ['column', 'row'],
+                    justifyContent: 'space-between',
+                    alignItems: ['baseline', 'center'],
+                    p: '23px 12px',
+                }}>
+                    <Flex mb={['16px', '0px']}>
+                        <Box mr="16px"><Icon name="facebook" size="24px" color="#017CFE" /></Box>
+                        <Box mr="16px"><Icon name="instagram" size="24px" color="#017CFE" /></Box>
+                        <Box mr="16px"><Icon name="twitter" size="24px" color="#017CFE" /></Box>
+                    </Flex>
+                    <Flex>
+                        <FooterNavs>
+                           <FooterNav>Nous Contacter</FooterNav>
+                           <FooterNav>Mentions Légales</FooterNav>
+                           <FooterNav>Plan du Site</FooterNav>
+                        </FooterNavs>
+                    </Flex>
+                </Flex>
+
+                <Flex sx={{
+                    fontSize: "11px",
+                    color: '#017CFE',
+                    flexDirection: ['column', 'row'],
+                    justifyContent: 'space-between',
+                    p: '23px 12px',
+                    borderTop: '1px solid',
+                    borderTopColor: 'gray400',
+                }}>
+                    <Box mb={['12px', '0']}>©2020 CAPITAL AZUR</Box>
+                    <Box>Conception et Développement: VOID</Box>
+                </Flex>
+
+            </Flex>
+        </Footer>
+    )
+}
--- src/components/Footer/stories/footer.stories.js
+++ src/components/Footer/stories/footer.stories.js
@@ -6,7 +6,8 @@ import { Text } from '../../Typography';
 import { Anchor } from '../../Anchor/Anchor';
 import { Navs, Nav } from '../../Navs';
 import { Icon} from '../../Icon';
-import { FooterVariant2 } from "../FooterVariant2"
+import { FooterVariant1 as _FooterVariant1 } from "../FooterVariant1"
+import { FooterVariant2 as _FooterVariant2 } from "../FooterVariant2"
 
 export const Simple = () => {
 
@@ -18,80 +19,6 @@ export const Simple = () => {
                 <Text textAlign="center" fontSize="14px" color="white">Footer - void</Text>
             </Footer>
         </Box>);
-}
-
-const FooterNav = props =>  <Nav sx={{
-    color: 'inherit',
-    p: 0,
-    px: [0, '16px'],
-    borderRight: ['0', '1px solid rgba(0,0,0,0.19)'],
-    m:0,
-    mb: ['12px', '0'],
-    '&:last-child' : {
-        borderRight: 0,
-        pr: 0,
-        m: 0
-    }
-    }}>{props.children}</Nav>
-
-const FooterNavs = props =>  <Navs sx={{
-        p: 0,
-        m:0,
-        width:"100%",
-        alignItems: 'baseline'
-        }}>{props.children}</Navs>
-
-export const CapitalAzur = () => {
-    return (<Box height="100vh" display="flex" flexDirection="column" justifyContent="flex-end">
-        <Footer sx={{
-            backgroundColor: '#fff',
-            fontFamily: 'montserrat',
-            display: 'flex',
-            flexDirection: 'column'
-        }}>
-            <Flex sx={{
-                width: ['100%', '100%', '900px', '1200px', '1900px'],
-                display: 'flex',
-                flexDirection: 'column',
-            }}>
-                <Flex sx={{
-                    fontSize: "14px",
-                    color: '#000000',
-                    flexDirection: ['column', 'row'],
-                    justifyContent: 'space-between',
-                    alignItems: ['baseline', 'center'],
-                    p: '23px 12px',
-                }}>
-                    <Flex mb={['16px', '0px']}>
-                        <Box mr="16px"><Icon name="facebook" size="24px" color="#017CFE" /></Box>
-                        <Box mr="16px"><Icon name="instagram" size="24px" color="#017CFE" /></Box>
-                        <Box mr="16px"><Icon name="twitter" size="24px" color="#017CFE" /></Box>
-                    </Flex>
-                    <Flex>
-                        <FooterNavs>
-                           <FooterNav>Nous Contacter</FooterNav>
-                           <FooterNav>Mentions Légales</FooterNav>
-                           <FooterNav>Plan du Site</FooterNav>
-                        </FooterNavs>
-                    </Flex>
-                </Flex>
-
-                <Flex sx={{
-                    fontSize: "11px",
-                    color: '#017CFE',
-                    flexDirection: ['column', 'row'],
-                    justifyContent: 'space-between',
-                    p: '23px 12px',
-                    borderTop: '1px solid',
-                    borderTopColor: 'gray400',
-                }}>
-                    <Box mb={['12px', '0']}>©2020 CAPITAL AZUR</Box>
-                    <Box>Conception et Développement: VOID</Box>
-                </Flex>
-
-            </Flex>
-        </Footer>
-    </Box>)
 }
 
 export const AWFFooter = () => {
@@ -147,7 +74,16 @@ export const AWFFooter = () => {
         </Box>);
 }
 
-export { FooterVariant2 };
+export const FooterVariant1 = () => (
+    <Box height="100vh" display="flex" flexDirection="column" justifyContent="flex-end">
+        <_FooterVariant1 />
+    </Box>);
+
+export const FooterVariant2 = () => (
+    <Box height="100vh" display="flex" flexDirection="column" justifyContent="flex-end">
+        <_FooterVariant2 />
+    </Box>
+    );
 
 export default {
     title: 'Footer'
