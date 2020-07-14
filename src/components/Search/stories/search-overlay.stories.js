df6b85c2c86b6829f8bc8f65504129225cf5ac92
Saber Ouarezzamen <our.saber@gmail.com>
2020-07-14 16:19:54 +0100

    #17461 Gatsby/Search Overlay

--- src/components/Search/stories/search-overlay.stories.js
+++ src/components/Search/stories/search-overlay.stories.js
@@ -11,6 +11,26 @@ export const Default = () => {
     const [query, setQuery] = React.useState(null);
 
     return (
+            <Box p="3rem" >
+
+                <Button borderRadius="rounded" outline="primary" onClick={() => setShow(true)}>show</Button>
+                {query && <Box>Searching for: {query}</Box>}
+
+                <SearchOverlay  onSubmit={q => { setQuery(q); setShow(false) }}
+                    onClose={() => setShow(false)}
+                    open={show} />
+
+            </Box>
+
+    );
+}
+
+export const RTL = () => {
+
+    const [show, setShow] = React.useState(false);
+    const [query, setQuery] = React.useState(null);
+
+    return (
         <DirectionManager dir="rtl">
             <Box p="3rem" dir="rtl" >
 
