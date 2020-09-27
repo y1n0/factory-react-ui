bdf9e8a2aa1f6bd3e4fcf8ac381cfa32968a402e
Hamza Bahlaouane <h.bahlaouane@void.fr>
2020-09-27 18:55:37 +0100

    Update packages.

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,7 @@ module.exports = {
     '@storybook/addon-knobs/register',
     '@storybook/addon-storysource',
     '@storybook/addon-notes/register',
-    '@storybook/addon-backgrounds/register'
+    '@storybook/addon-backgrounds/register',
+    '@storybook/addon-viewport'
   ],
 };
