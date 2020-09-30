aca03142cb2688546873c8118e90ef723b8a96df
Abdelkoddous <a.elidrissi@void.fr>
2020-09-30 17:33:52 +0100

    Convert predefined shadows array to object
    so they can be merged with the custom theme without being overridden

--- src/theme/constants/shadows.js
+++ src/theme/constants/shadows.js
@@ -5,12 +5,12 @@
  * Colors
  */
 
- const shadows = [
-     '0 1px 1px 0 rgba(8, 11, 14, 0.1)',
-     '0 3px 3px -1px rgba(8, 11, 14, 0.1)',
-     '0 6px 6px -1px rgba(8, 11, 14, 0.1)',
-     '0 16px 16px -1px rgba(8, 11, 14, 0.1)',
-     '0 32px 40px -1px rgba(8, 11, 14, 0.1)'
-];
+ const shadows = {
+    0: '0 1px 1px 0 rgba(8, 11, 14, 0.1)',
+    1: '0 3px 3px -1px rgba(8, 11, 14, 0.1)',
+    2: '0 6px 6px -1px rgba(8, 11, 14, 0.1)',
+    3: '0 16px 16px -1px rgba(8, 11, 14, 0.1)',
+    4: '0 32px 40px -1px rgba(8, 11, 14, 0.1)',
+  };
 
  export default shadows;
